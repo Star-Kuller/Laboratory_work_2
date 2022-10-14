75269e022ab748e061f84e963610b5ec61b075cb
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-10-15 01:35:07 +0300

    2

--- Lab_work_2/Exercise/Exercise/Exercise.cpp
+++ Lab_work_2/Exercise/Exercise/Exercise.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void N_1(int number_of_lines)
 {
+    cout << "------------------------------------------------------\n";
     for (int i = 0; i < number_of_lines; i++) {
         for (int j = 0; j < number_of_lines-i; j++) {
             cout << " ";
@@ -13,10 +14,12 @@ void N_1(int number_of_lines)
         cout << "\n";
     }
     cout << endl << "Возвращение в меню...\n";
+    cout << "------------------------------------------------------\n";
 }
 
 void N_2()
 {
+    cout << "------------------------------------------------------\n";
     int UserInput;
     long long Number;
     do
@@ -32,10 +35,12 @@ void N_2()
         }
     } while (!UserInput == 0);
     cout << endl << "Возвращение в меню...\n";
+    cout << "------------------------------------------------------\n";
 }
 
 void N_3()
 {
+    cout << "------------------------------------------------------\n";
     float deposit, percent, intermediate;
     int years;
     cout << "Введите изначальный вклад: "; cin >> deposit; 
@@ -67,12 +72,13 @@ void N_3()
         cout << intermediate  << "\n";
     }
     cout << "Итог:" << intermediate << "\n";
-    cout << "------------------------------------------------------\n";
     cout << endl << "Возвращение в меню...\n";
+    cout << "------------------------------------------------------\n";
 }
 
 void N_4()
 {
+    cout << "------------------------------------------------------\n";
     float price, payment, percent, intermediate, summ, x;
     int years;
     cout << "Введите стоимость квартиры: "; cin >> price;
@@ -92,11 +98,67 @@ void N_4()
     cout << "------------------------------------------------------\n";
     cout << "Общая сумма выплат: " << summ + payment << endl;
     cout << endl << "Возвращение в меню...\n";
+    cout << "------------------------------------------------------\n";
+}
+
+void N_5()
+{
+    int pound_A, pound_B, shilling_A, shilling_B, penny_A, penny_B, pound, shilling, penny;
+    char dot;
+    string YorN;
+    do {
+        cout << "\nВведите первую сумму фунтов: "; cin >> pound_A; cin.get(dot); cin >> shilling_A; cin.get(dot); cin >> penny_A;
+        cout << "Введите вторую сумму фунтов: "; cin >> pound_B; cin.get(dot); cin >> shilling_B; cin.get(dot); cin >> penny_B;
+        pound = pound_A + pound_B;
+        shilling = shilling_A + shilling_B;
+        penny = penny_A + penny_B;
+        shilling += penny / 12;
+        penny = penny % 12;
+        pound += shilling / 20;
+        shilling = shilling % 20;
+        cout << "\nОбщая сумма равна: " << pound << "." << shilling << "." << penny << " фунтов\n";
+        cout << "\n продолжить (y/n)?\n"; cin >> YorN;
+    } while (YorN == "y" | YorN == "Y" | YorN == "Yes" | YorN == "yes" | YorN == "YES");
+    cout << endl << "Возвращение в меню...\n";
+    cout << "------------------------------------------------------\n";
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    N_4();
+    int selected, N_1_lines;
+    N_1_lines = 20;
+    string YorN;
+    while (true) {
+        cout << "\n0 - Завершить работу\n1,2,3,4,5 - доступные задания\n";
+        cout << "Выберите задание или 0: "; cin >> selected;
+        switch (selected)
+        {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            cout << "\nЖелаете изминить кол-во строк (y/n)?\n"; cin >> YorN;
+            if (YorN == "y" | YorN == "Y" | YorN == "Yes" | YorN == "yes" | YorN == "YES") {
+                cout << "\nВведите новое количество строк: "; cin >> N_1_lines;
+            }
+            N_1(N_1_lines);
+            break;
+        case 2:
+            N_2();
+            break;
+        case 3:
+            N_3();
+            break;
+        case 4:
+            N_4();
+            break;
+        case 5:
+            N_5();
+            break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
